1f8eb474b2ac5768bdd3bd9b7a42ce11490a0450
killersuresh93 <95585683+killersuresh93@users.noreply.github.com>
2023-08-14 12:34:45 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Social-Care-Hub
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/stackblitz-starters-bqlzbx)
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import PatientDetails from './components/patientDetails';
+import './style.css';
+
+export default function App() {
+  return (
+    <div className="container-fluid">
+      <PatientDetails />
+    </div>
+  );
+}
